785e5e6a04a0c687aa1509ffeedc288f1175c5dd
Keenan Pautler <kpautler4@gmail.com>
2019-02-20 18:59:04 -0700

    checks for quantity and displays items

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# bamazon
+# Bamazon
+
+## A node Application 
+
+``` 
+git clone git@github.com:kepa4/bamazon.git 
+cd bamazon
+npm install
+```
+use mysql to run schema.sql and seeds.sql
+
+### to run the application: 
+`node bamazon.js`
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,37 +24,54 @@ connection.connect(function(err) {
 function afterConnection() {
   connection.query('SELECT * FROM products', function(err, res) {
     if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log('==================================================================');
+      console.log(
+        'item id:',
+        res[i].item_id,
+        res[i].product_name,
+        'price:',
+        res[i].price,
+        'quantity:',
+        res[i].stock_quantity,
+      );
+    }
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: 'Enter the Id of the product you want to buy',
+          name: 'product_id',
+        },
+        {
+          type: 'input',
+          message: 'How many would you like to buy?',
+          name: 'quantity',
+        },
+      ])
+      .then(function(answers) {
+        connection.query(
+          'Select price, stock_quantity FROM products WHERE item_id = ?',
+          [answers.product_id],
+          function(err, res) {
+            if (err) throw err;
+            if (res[0].stock_quantity - answers.quantity <= 0) {
+              console.log('insufficient stock sorry');
+              connection.end();
+            } else {
+              console.log('This will cost $' + res[0].price * answers.quantity);
+              connection.query(
+                'UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?',
+                [answers.quantity, answers.product_id],
+                function(err, res) {
+                  if (err) throw err;
+                  console.log('Database updated');
+                  connection.end();
+                },
+              );
+            }
+          },
+        );
+      });
   });
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        message: 'Enter the Id of the product you want to buy',
-        name: 'product_id',
-      },
-      {
-        type: 'input',
-        message: 'How many would you like to buy?',
-        name: 'quantity',
-      },
-    ])
-    .then(function(answers) {
-      connection.query(
-        'UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?',
-        [answers.quantity, answers.product_id],
-        function(err, res) {
-          if (err) throw err;
-          console.log('Database updated');
-        },
-      );
-
-      connection.query(
-        'Select price FROM products WHERE item_id = ?',
-        [answers.product_id],
-        function(err, res) {
-          if (err) throw err;
-          console.log('This will cost $' + res[0].price * answers.quantity);
-        },
-      );
-    });
 }
